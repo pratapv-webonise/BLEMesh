2a06b13bdc5da16c3ec47576a84f0fe8dc0bf2d3
Pratap Vhatkar <pratapv@weboniselab.com>
2014-10-30 10:30:43 +0530

    new setup

---
+++ BLEMesh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEMesh
+//
+//  Created by Mac-4 on 30/10/14.
+//  Copyright (c) 2014 WeboniseLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLEMesh/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BLEMesh
+//
+//  Created by Mac-4 on 30/10/14.
+//  Copyright (c) 2014 WeboniseLab. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
